248367c4988426ea1159595cd8171044d2f3990a
yhironaka <yhironaka@gmail.com>
2015-10-11 09:14:03 +0900

    ROTATE=1を同時にビルドできるようにMakefileR1とbuildシェルスクリプトを追加

--- main.h
+++ main.h
@@ -11,7 +11,9 @@ main.h
 #define TYPE 0 // 通常
 //#define TYPE 1 // マトリクスLEDの位置、間違えた時
 
+#ifndef ROTATE
 #define ROTATE 2
+#endif
 //#define ROTATE 1 // 右回転
 //#define ROTATE 2 // 上下反転
 //#define ROTATE 3 // 左回転
